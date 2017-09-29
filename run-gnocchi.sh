eabe892eef55fb11c2fce855a27ec591e70fb10e
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-09-29 19:04:38 -0300

    improve run-gnocchi.sh and add conf example

--- run-gnocchi.sh
+++ run-gnocchi.sh
@@ -2,7 +2,8 @@
 printf "[indexer]\nurl = postgresql://${POSTGRESQL_USER}:${POSTGRESQL_PASSWORD}@${POSTGRESQL_SERVICE_HOST}:${POSTGRESQL_SERVICE_PORT}/${POSTGRESQL_DATABASE}\n" > /etc/gnocchi/gnocchi.conf
 printf "[storage]\ncoordination_url = redis://:${REDIS_PASSWORD}@${REDIS_SERVICE_HOST}:${REDIS_SERVICE_PORT}\n" >> /etc/gnocchi/gnocchi.conf
 printf "[incoming]\ndriver = redis\nredis_url = redis://:${REDIS_PASSWORD}@${REDIS_SERVICE_HOST}:${REDIS_SERVICE_PORT}\n" >> /etc/gnocchi/gnocchi.conf
-printf "[statsd]\nresource_id = 0a8b55df-f90f-491c-8cb9-7cdecec6fc26\n" >> /etc/gnocchi/gnocchi.conf
+printf "[statsd]\nresource_id = 0a8b55df-f90f-491c-8cb9-7cdecec6fc26\nflush_delay = 1\n" >> /etc/gnocchi/gnocchi.conf
+printf "[cors]\nallowed_origin = *\n" >> /etc/gnocchi/gnocchi.conf
 gnocchi-upgrade || true
 gnocchi-metricd &
 gnocchi-statsd &
